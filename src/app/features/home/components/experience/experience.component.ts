73d7d9fa0eaf1a956a190bd024dcc2cd441fd16b
SalvatoreDiGenua <saldigenua@gmail.com>
2023-04-06 17:56:55 +0200

    up experiences

--- src/app/features/home/components/experience/experience.component.ts
+++ src/app/features/home/components/experience/experience.component.ts
@@ -25,14 +25,25 @@ export class ExperienceComponent implements OnInit {
   }
 
   getExperiences() {
-    this.experiences.push({
-      company: 'ACCA Software',
-      employment: 'Web Developer',
-      description: 'Frontend Developer che si occupa di progettazione di interfacce web e di user-experience. Backend developer che si occupa dello sviluppo server delle applicazioni.',
-      permanence: '2019 / ad oggi',
-      siteUrl: 'https://acca.it',
-      faviconUrl: '/assets/experience-icons/acca/logo_acca.svg',
-      stack: ['JavaScript', 'HTML5', 'CSS3', 'Angular(2+)', 'Typescript', 'Node']
-    });
-  }  
+    this.experiences = [
+      {
+        company: 'SCAI Tecno',
+        employment: 'Web Developer',
+        description: 'Frontend Developer che si occupa di progettazione di interfacce web e di user-experience.',
+        permanence: '2023 / in corso',
+        siteUrl: 'https://www.grupposcai.it/company/scai-tecno/',
+        faviconUrl: '/assets/experience-icons/scai_tecno/logo_scai_tecno.svg',
+        stack: ['JavaScript', 'HTML5', 'CSS3', 'Angular(2+)', 'Typescript', 'Bootstrap']
+      },
+      {
+        company: 'ACCA Software',
+        employment: 'Web Developer',
+        description: 'Frontend Developer che si occupa di progettazione di interfacce web e di user-experience. Backend developer che si occupa dello sviluppo server delle applicazioni.',
+        permanence: '2019 / 2023',
+        siteUrl: 'https://acca.it',
+        faviconUrl: '/assets/experience-icons/acca/logo_acca.svg',
+        stack: ['JavaScript', 'HTML5', 'CSS3', 'Angular(2+)', 'Typescript', 'Node']
+      }
+    ]
+  }
 }

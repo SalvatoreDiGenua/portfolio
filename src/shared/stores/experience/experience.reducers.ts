34f84997e9bb86601f888298ff7236a15dbe56ad
Salvatore Di Genua <saldigenua@gmail.com>
2024-06-29 14:04:59 +0200

    update default experience

--- src/shared/stores/experience/experience.reducers.ts
+++ src/shared/stores/experience/experience.reducers.ts
@@ -4,19 +4,19 @@ import { Experience } from "src/shared/models/sdg-portfolio-models";
 const defaultExperience: Experience[] = [
     {
         company: 'SCAI Tecno',
-        employment: 'Web Developer',
+        employment: 'Frontend Developer | Angular',
         permanence: '2023 / in progress',
         siteUrl: 'https://www.grupposcai.it/company/scai-tecno/',
         faviconUrl: '/assets/experience-icons/scai_tecno/logo_scai_tecno.svg',
-        stack: ['JavaScript', 'HTML5', 'CSS3', 'Angular(2+)', 'Typescript', 'NgRx', 'RxJs', 'Bootstrap']
+        stack: ['JavaScript', 'HTML5', 'CSS3', 'Angular(2+)', 'Typescript', 'NgRx', 'RxJs', 'Bootstrap', 'Cypress']
     },
     {
         company: 'ACCA Software',
-        employment: 'Web Developer',
+        employment: 'Full Stack Developer',
         permanence: '2019 / 2023',
         siteUrl: 'https://acca.it',
         faviconUrl: '/assets/experience-icons/acca/logo_acca.svg',
-        stack: ['JavaScript', 'HTML5', 'CSS3', 'Angular(2+)', 'Typescript', 'NgRx', 'RxJs', 'NodeJS']
+        stack: ['JavaScript', 'HTML5', 'CSS3', 'Angular(2+)', 'Typescript', 'NgRx', 'RxJs', 'NodeJS', 'ExpressJS', 'SocketIO', 'MySQL']
     }
 ]
 
